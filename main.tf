beff7776724eae4e1aa11efe5e2146efd2206189
Malcolm Matalka <malcolm.matalka@acsl.se>
2024-07-24 10:51:21 -0400

    test

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+}
